f47abfd0af337249e49a8e7bcbddff46a0631925
ahr <abdulhafizr>
2021-02-10 23:06:10 +0700

    | Install rn-placeholder | Testing rn-placeholder

--- App.js
+++ App.js
@@ -1,114 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { 
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  Fade 
+} from 'rn-placeholder';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
+  const [loadingPlaceholder, setLoadingPlaceholder] = useState(true);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingPlaceholder(false);
+      }, 3000)
+  }, [])
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={{marginTop: 30, marginHorizontal: 20,}}>
+        {
+          loadingPlaceholder ? 
+          (
+            <View>
+              <Placeholder
+                Animation={Fade}
+                Right={PlaceholderMedia}
+              />
+              <Placeholder
+                Animation={Fade}
+                style={{marginTop: 20}}
+              >
+                <PlaceholderLine width={30} />
+
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+
+              </Placeholder>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+          ) : 
+          (
+            <View>
+              <Placeholder
+                Right={PlaceholderMedia}
+              />
+              <Placeholder
+                style={{marginTop: 20}}
+              >
+                <PlaceholderLine width={30} />
+
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+                <PlaceholderLine style={styles.messages} />
+
+              </Placeholder>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          )
+        }
+      </View>
+    </ScrollView>
+  )
+}
+
+export default App
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  messages: {
+    height: 132,
+    borderRadius: 5,
+  }
+})
